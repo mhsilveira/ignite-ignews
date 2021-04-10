367191617e7facc2ffe7894b6b7e42af186ad691
Matheus <blanchimaah@gmail.com>
2021-04-10 13:23:14 -0300

    Commit previo ao CMS

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,26 +1,30 @@
 import { FaGithub } from 'react-icons/fa'
-import { FiX} from 'react-icons/fi'
-
+import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client';
 import styles from './styles.module.scss';
 
-export function SignInButton(){
-  const isUserLoggedIn = true;
+export function SignInButton() {
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
-    <button 
-    type="button"
-    className={styles.signInButton}
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
     >
-      <FaGithub color='#04d361'/>
-      Matheus Silveira
-      <FiX color="#737380" className={styles.closeIcon}/>
+      <FaGithub color='#04d361' />
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button 
-    type="button"
-    className={styles.signInButton}
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() =>
+        signIn('github')
+      }
     >
-      <FaGithub color='#eba417'/>
+      <FaGithub color='#eba417' />
       Sign in with Github
     </button>
   )
